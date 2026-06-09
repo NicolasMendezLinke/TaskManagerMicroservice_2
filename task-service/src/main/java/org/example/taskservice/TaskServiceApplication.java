5f02b2b4f87cee579b7e6188dde00bbd51d6a09e
Nicolas Mendez Martins <mendezz.nick@gmail.com>
2026-06-09 15:09:00 -0300

    refatoração e introdução ao eureka

--- task-service/src/main/java/org/example/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/org/example/taskservice/TaskServiceApplication.java
@@ -3,9 +3,11 @@ package org.example.taskservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableRabbit
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
